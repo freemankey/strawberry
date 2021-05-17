afe836d29cd8b51eb193cfe55ae01fd76c6b62f4
freemankey <yta2489@gmail.com>
2021-05-17 12:21:12 +0900

    ロードセル制御プログラム初期

---
+++ loadcell_ini/sketch_apr27a.ino
@@ -0,0 +1,26 @@
+#include "HX711.h"
+ 
+//DT,SCKは実際の配線に応じる
+const int DT_PIN = A3;
+const int SCK_PIN = A1;
+//キャリブレーション用変数。ロードセルに依存
+const int calibrate_dec = -944;
+HX711 scale;
+ 
+void setup() {
+Serial.begin(9600);
+scale.begin(DT_PIN, SCK_PIN);
+// scale.set_scale();の(calibrate_dec)内にロードセルに応じた値を入れる
+// 使用したキッチンスケールだと-944がいい感じの値
+scale.set_scale(calibrate_dec);
+scale.tare();
+}
+ 
+void loop() {
+// scale.get_units(10)の値を小数点以下2桁まで表示
+//scale.get_units(10), 1にすれば1桁
+Serial.println(scale.get_units(10), 2);
+scale.power_down();
+delay(50);
+scale.power_up();
+}
