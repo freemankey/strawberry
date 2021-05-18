35532b956b76af6be03cc4a2a112604051e9541e
haruki0807 <haruki08070743@gmail.com>
2021-05-18 22:43:04 +0900

    すぐに消えるバグを修正

--- smart_measure/smart_measure.ino
+++ smart_measure/smart_measure.ino
@@ -6,11 +6,11 @@ const int SCK_PIN = 9;
 const int DIN_PIN = 7;
 //キャリブレーション用変数。ロードセルに依存
 const int calibrate_dec = -685;
-float b_weight = -1.0;//基準値_base
-float p_weight = -1.0;//前回値_primary
-float n_weight = -1.0;//現在値_now
+float b_weight = -1.0; //基準値_base
+float p_weight = -1.0; //前回値_primary
+float n_weight = -1.0; //現在値_now
 float ichigo_weight = 0;
-float error = 0.1;//誤差をどれだけ許容するか
+float error = 0.1; //誤差をどれだけ許容するか
 int btn;
 
 HX711 scale;
@@ -36,10 +36,12 @@ void loop()
     else
     {
         n_weight = scale.get_units(10);
-        if (abs(n_weight - p_weight) < error)/*現在値が前回値よりerror値よりも小さくなっていればイチゴが取られたと判定する*/
+        if (abs(n_weight - p_weight) < error && n_weight - b_weight >= 5) /*現在値が前回値よりerror値よりも小さくなっていればイチゴが取られたと判定する*/
         {
-            ichigo_weight = n_weight - b_weight;　//イチゴの値を更新＊ただし，この値は数ループの間しか保持できない
-            b_weight = n_weight;　//基準値をリセット
+            ichigo_weight = n_weight - b_weight;
+            　 //イチゴの値を更新＊ただし，この値は数ループの間しか保持できない
+                b_weight = n_weight;
+            　 //基準値をリセット
         }
         p_weight = n_weight;
 
@@ -49,15 +51,16 @@ void loop()
         ・itigo_weightの値を次のイチゴが取り除かれるまで保持　ichigo_weigtの中身が再び一定値以上なるまで値を保持するとか
        　どっちかできません？
         */
-        
+
         /*デバッグ用
         Serial.println(n_weight);
         Serial.println(b_weight);
         Serial.println(p_weight);
         */
-        Serial.println(n_weight - b_weight);　//現在値と基準値の誤差を表示する
-        Serial.print("     weight:  ");
-        Serial.println(ichigo_weight);//
+        Serial.println(n_weight - b_weight);
+        　 //現在値と基準値の誤差を表示する
+            Serial.print("     weight:  ");
+        Serial.println(ichigo_weight); //
 
         scale.power_down();
         delay(100);
