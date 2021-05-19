dc44b8e4ec7c79464c63c7670c7fbcaebad81444
haruki0807 <haruki08070743@gmail.com>
2021-05-19 16:30:09 +0900

    '変数名変更,正負を修正

--- smart_measure/smart_measure.ino
+++ smart_measure/smart_measure.ino
@@ -1,32 +1,40 @@
 #include "HX711.h"
 #include <Adafruit_NeoPixel.h>
 
+//LEDによる点灯に関する変数
 const int LED_PIN = 2;     // 制御するピン
 const int LED_COUNT = 150; // LEDの数
 
 //DT,SCKは実際の配線に応じる
-const int DT_PIN = 8;
-const int SCK_PIN = 9;
-const int DIN_PIN = 7;
+const int DT_PIN = 6;
+const int SCK_PIN = 7;
+const int RST_PIN = 5;
+int RST_val;
+
 //キャリブレーション用変数。ロードセルに依存
 const int calibrate_dec = -685;
-float b_weight = -1.0; //基準値_base
-float p_weight = -1.0; //前回値_primary
-float n_weight = -1.0; //現在値_now
-float ichigo_weight = 0;
-float error = 0.1; //誤差をどれだけ許容するか
-int btn;
+
+//イチゴの重量を測定に関する変数
+float b_weight = -1.0;   //基準値_base
+float p_weight = -1.0;   //前回値_primary
+float n_weight = -1.0;   //現在値_now
+float ichigo_weight = 0; //イチゴの重量
+float error = 0.1;       //誤差をどれだけ許容するか
 
 HX711 scale;
 Adafruit_NeoPixel pixels(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 void setup()
 {
     Serial.begin(9600);
-    pinMode(DIN_PIN, INPUT_PULLUP);
+    pinMode(RST_PIN, INPUT_PULLUP);
     scale.begin(DT_PIN, SCK_PIN);
     scale.set_scale(calibrate_dec);
     scale.tare();
     pixels.begin();
+
+    pixels.clear();
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
 }
 
 void lit_LED(int ichigo_weight)
@@ -40,30 +48,34 @@ void lit_LED(int ichigo_weight)
         pixels.setPixelColor(0, pixels.Color(0, 0, 255)); // 0番目の色を青に変える
     else if (5 <= ichigo_weight && ichigo_weight <= 9)
         pixels.setPixelColor(0, pixels.Color(255, 255, 0)); // 0番目の色を黄色に変える
-    else　//それ以外
-        pixels.setPixelColor(0, pixels.Color(255,255,255); // 0番目の色を白に変える
-
+    else if (ichigo_weight > 0)
+        pixels.setPixelColor(0, pixels.Color(255, 255, 255)); // 0番目の色を白に変える
+    else
+        pixels.setPixelColor(0, pixels.Color(0, 0, 0));
     pixels.show();
 }
 void loop()
 {
-    btn = digitalRead(DIN_PIN);
+    RST_val = digitalRead(RST_PIN);
     n_weight = scale.get_units(10);
-    if (btn == 0) //リセット時
+    if (RST_val == 0) //リセット時
     {
         Serial.println("reset!!");
         b_weight = n_weight;
+        ichigo_weight = 0;
+
+        pixels.clear();
+        pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+        pixels.show();
     }
     else
     {
         n_weight = scale.get_units(10);
-        if (abs(n_weight - p_weight) < error && n_weight - b_weight >= 5) /*現在値が前回値よりerror値よりも小さくなっていればイチゴが取られたと判定する*/
+        if (abs(n_weight - p_weight) < error && abs(n_weight - b_weight) >= 5) /*現在値が前回値よりerror値よりも小さくなっていればイチゴが取られたと判定する*/
         {
-            ichigo_weight = n_weight - b_weight;
-            　 //イチゴの値を更新＊ただし，この値は数ループの間しか保持できない
-                b_weight = n_weight;
-            　 //基準値をリセット
-                lit_LED(ichigo_weight);
+            ichigo_weight = -(n_weight - b_weight); //イチゴの値を更新＊ただし，この値は数ループの間しか保持できない
+            b_weight = n_weight;                    //基準値をリセット
+            lit_LED(ichigo_weight);
         }
         p_weight = n_weight;
 
@@ -79,10 +91,10 @@ void loop()
         Serial.println(b_weight);
         Serial.println(p_weight);
         */
-        Serial.println(n_weight - b_weight);
-        　 //現在値と基準値の誤差を表示する
-            Serial.print("     weight:  ");
-        Serial.println(ichigo_weight); //
+        Serial.println(n_weight - b_weight); //現在値と基準値の誤差を表示する
+        Serial.print("    ichigo weight:  ");
+        Serial.println(ichigo_weight);
+        Serial.println();
 
         scale.power_down();
         delay(100);
