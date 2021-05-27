35fda6483700006808a1829763b96f669e190dd5
haruki0807 <haruki08070743@gmail.com>
2021-05-27 17:02:46 +0900

    '条件式を修正'

--- smart_measure/smart_measure.ino
+++ smart_measure/smart_measure.ino
@@ -19,7 +19,7 @@ float b_weight = -1.0;   //基準値_base
 float p_weight = -1.0;   //前回値_primary
 float n_weight = -1.0;   //現在値_now
 float ichigo_weight = 0; //イチゴの重量
-float error = 0.1;       //誤差をどれだけ許容するか
+float error = 0.5;       //誤差をどれだけ許容するか
 
 HX711 scale;
 Adafruit_NeoPixel pixels(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -42,19 +42,19 @@ void lit_LED(float ichigo_weight, bool flag)
     pixels.clear();
     if (29 <= ichigo_weight && ichigo_weight <= 33)       //Lサイズの場合
         pixels.setPixelColor(3, pixels.Color(255, 0, 0)); // 0番目の色を赤に変える
-    else if (20 <= ichigo_weight && ichigo_weight <= 28)  //Mサイズの場合
+    else if (20 <= ichigo_weight && ichigo_weight <29)  //Mサイズの場合
         pixels.setPixelColor(2, pixels.Color(0, 255, 0)); // 0番目の色を緑に変える
-    else if (16 <= ichigo_weight && ichigo_weight <= 19)  //Sサイズの場合
+    else if (16 <= ichigo_weight && ichigo_weight <20)  //Sサイズの場合
         pixels.setPixelColor(1, pixels.Color(0, 0, 255)); // 0番目の色を青に変える
-    else if (12 <= ichigo_weight && ichigo_weight <= 15)
+    else if (12 <= ichigo_weight && ichigo_weight < 16)
         pixels.setPixelColor(0, pixels.Color(255, 255, 0)); // 0番目の色を黄色に変える
-    else if ((0 <= ichigo_weight && ichigo_weight <= 11) || ichigo_weight >= 33)
-        pixels.setPixelColor(4          , pixels.Color(255, 255, 255)); // 0番目の色を白に変える
-    else if (ichigo_weight < 0)
-    {
-        for (int i = 0; i < 5; i++)
-            pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    }
+    else
+        pixels.setPixelColor(4, pixels.Color(255, 255, 255)); // 0番目の色を白に変える
+    // else if (ichigo_weight < 0)
+    // {
+    //     for (int i = 0; i < 5; i++)
+    //         pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    // }
     pixels.show();
     if (!flag)
     {
@@ -89,16 +89,19 @@ void loop()
     else
     {
         n_weight = scale.get_units(10);
-        if (abs(n_weight-p_weight)<error &&abs(n_weight - b_weight) >= 5) /*現在値が前回値よりerror値よりも小さくなっていればイチゴが取られたと判定する*/
+        if (abs(n_weight - p_weight) < error&&abs(n_weight - b_weight) >= 5) /*現在値が前回値よりerror値よりも小さくなっていればイチゴが取られたと判定する*/
         {
-            ichigo_weight = -(n_weight - b_weight); //イチゴの値を更新＊ただし，この値は数ループの間しか保持できない
-            b_weight = n_weight;                    //基準値をリセット
-                        lit_LED(ichigo_weight, true);
+            ichigo_weight = abs(-(n_weight - b_weight)); //イチゴの値を更新＊ただし，この値は数ループの間しか保持できない
+            b_weight = n_weight;                         //基準値をリセット
+            lit_LED(ichigo_weight, true);
         }
-        else if(abs(n_weight-b_weight)>=5)
-        lit_LED(-(n_weight-b_weight),false);
+        else if (abs(n_weight - b_weight) >= 5)
+            lit_LED(abs(-(n_weight - b_weight)), false);
+        else lit_LED(ichigo_weight,true);
         p_weight = n_weight;
         Serial.println(n_weight - b_weight); //現在値と基準値の誤差を表示する
+        Serial.println(n_weight);
+        Serial.println(b_weight);
         Serial.print("    ichigo weight:  ");
         Serial.println(ichigo_weight);
         Serial.println();
